eca00b58a76d15a145dcffa9e4f3a933f7c34a3a
Ramón Carnero Rojo <blablar2000@gmail.com>
2018-02-12 21:06:11 +0100

    Error corregido

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -14,7 +14,7 @@ public enum Cell {
     private String symbol;
 
     private Cell(final String initialSymbol) {
-        this.symbol = 12;
+        this.symbol = initialSymbol;
     }
 
 	// Override function for easy printing of cell's symbol
